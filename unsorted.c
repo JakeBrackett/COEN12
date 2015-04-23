bf313f4f28f4d895a1e179f2da7975e7dae5495b
jacob ryan brackett <jbracket@dcmac15.dc.engr.scu.edu>
2015-04-23 11:30:03 -0700

    started report.txt, and done with all else

--- unsorted.c
+++ unsorted.c
@@ -1,99 +1,179 @@
-//
-//  set.c
-//  
-//
-//  Created by Jacob Brackett on 4/8/15.
-//
-//
+/*
+ * File:        unsorted.c
+ *
+ * Copyright:	2015, Darren C. Atkinson
+ *
+ * Description: This file contains the public and private function and type
+ *              definitions for a set abstract data type for strings.  A
+ *              set is an unordered collection of distinct elements.
+ *
+ *              This implementation uses an unsorted array to store the
+ *              elements.  Insertion, deletion, and determining membership
+ *              are all linear time.
+ */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <assert.h>
-#include <string.h>
-#include "set.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <assert.h>
+# include <stdbool.h>
+# include "set.h"
 
-typedef struct set {
-    int count;
-    int length;
-    char ** elts;
-} SET;
+struct set {
+    int count;          /* number of elements in array */
+    int length;         /* length of allocated array   */
+    char **data;        /* allocated array of elements */
+};
 
-//mallocs space for a set, ititializes it and returns a pointer to the set
-// O(1)
-SET *createSet(int maxElts){
-    SET *sp;
-    sp = malloc(sizeof(SET));
-    assert(sp != NULL);
-    sp->count = 0;
-    sp->length = maxElts;
-    sp->elts = malloc(sizeof(char *) * maxElts);
-    assert(sp->elts != NULL);
-    return sp;
-}
 
-//frees the memory associated with a SET and the elts in it
-// O(1)
-void destroySet(SET *sp){
+/*
+ * Function:    findElement (private)
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Use sequential search to check if ELT is present in the set
+ *		SP and return its location if present, and -1 if it is not
+ *		present.
+ */
+
+static int findElement(SET *sp, char *elt)
+{
     int i;
-    assert(sp != NULL);
-    for(i = 0; i < sp->count; i++){
-        free(sp->elts[i]);
-    }
-    free(sp->elts);
-    free(sp);
-}
 
-//private function to search a SET for a specific elt
-// O(n)
-static int findElement(SET *sp, char *elt){
-    int i;
-    for(i=0; i<sp->count;i++){
-        if(strcmp(elt, sp->elts[i]) == 0)
+
+    for (i = 0; i < sp->count; i ++)
+        if (strcmp(sp->data[i], elt) == 0)
             return i;
-    }
+
     return -1;
 }
 
-//returns the number of Elements in a set
-// O(1)
-int numElements(SET *sp){
+
+/*
+ * Function:    createSet
+ *
+ * Complexity:  O(1)
+ *
+ * Description: Return a pointer to a new set with a maximum capacity of
+ *		MAXELTS.
+ */
+
+SET *createSet(int maxElts)
+{
+    SET *sp;
+
+
+    sp = malloc(sizeof(SET));
+    assert(sp != NULL);
+
+    sp->data = malloc(sizeof(char *) * maxElts);
+    assert(sp->data != NULL);
+
+    sp->count = 0;
+    sp->length = maxElts;
+
+    return sp;
+}
+
+
+/*
+ * Function:    destroySet
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Deallocate memory associated with the set pointed to by SP.
+ */
+
+void destroySet(SET *sp)
+{
+    int i;
+
+
+    assert(sp != NULL);
+
+    for (i = 0; i < sp->count; i ++)
+        free(sp->data[i]);
+
+    free(sp->data);
+    free(sp);
+}
+
+
+/*
+ * Function:    numElements
+ *
+ * Complexity:  O(1)
+ *
+ * Description: Return the number of elements in the set pointed to by SP.
+ */
+
+int numElements(SET *sp)
+{
     assert(sp != NULL);
     return sp->count;
 }
 
-//returns true if elt is found in the set sp
-// O(n)
-bool hasElement(SET *sp, char *elt){
+
+/*
+ * Function:    hasElement
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Check if ELT is present in the set pointed to by SP.
+ */
+
+bool hasElement(SET *sp, char *elt)
+{
     assert(sp != NULL && elt != NULL);
-    return findElement(sp,elt) != -1;
+    return findElement(sp, elt) != -1;
 }
 
-// adds elt to SET sp if elt is not already there and there is space in the set, returns true if the operation was successful
-// O(n)
-bool addElement(SET *sp, char *elt){
+
+/*
+ * Function:    addElement
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Add ELT to the set pointed to by SP, and return whether the
+ *		set changed.  A new element is added to the end of the
+ *		array.
+ */
+
+bool addElement(SET *sp, char *elt)
+{
     assert(sp != NULL && elt != NULL);
-    if(findElement(sp,elt) != -1 || sp->count == sp->length)
+
+    if (sp->count == sp->length || findElement(sp, elt) != -1)
         return false;
+
     elt = strdup(elt);
     assert(elt != NULL);
-    sp->elts[sp->count++] = elt;
+    sp->data[sp->count ++] = elt;
     return true;
 }
 
-//Removes an element from a set, returns true if element was removed
-// O(n)
-bool removeElement(SET *sp, char *elt){
-    int index;
+
+/*
+ * Function:    removeElement
+ *
+ * Complexity:  O(n)
+ *
+ * Description: Remove ELT from the set pointed to by SP, and return
+ *		whether the set changed.  A element is deleted by replacing
+ *		it with the last element in the array.
+ */
+
+bool removeElement(SET *sp, char *elt)
+{
+    int locn;
+
+
     assert(sp != NULL && elt != NULL);
-    index = findElement(sp, elt);
-    if(index == -1)
+
+    if ((locn = findElement(sp, elt)) == -1)
         return false;
-    free(sp->elts[index]);
-    sp->elts[index] = sp->elts[sp->count-1];
-    sp->count--;
+
+    free(sp->data[locn]);
+    sp->data[locn] = sp->data[-- sp->count];
     return true;
 }
-
-
-
